b2529d74718ab28ed63231f47975db1b402b2ddd
Tajrus Lipscomb Jr. <tajrus_lipscomb1@my.vcccd.edu>
2023-10-01 22:54:32 -0700

    Males to Females

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 def main():
-    """
-    ##################################################
-    Comlete your code here
-    Use m_perc and f_perc for your results
-    ##################################################
-    """
+    num_males = int(input('Enter the number of male students: 15'))
+    num_females = int(input('Enter the number of female students: 19'))
 
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
+    total_students = num_males + num_females
+    m_perc = (num_males / total_students) * 100
+    f_perc = (num_females / total_students) * 100
+
+    print(f'The total number of students:\t{total_students}')
+    print(f'The number of males and females:\t{num_males}\t{num_females}')
+    print(f'The percentage of males and females:\t{m_perc:.2f}%\t\t{f_perc:.2f}%')
+
     return m_perc, f_perc
-
 
 if __name__ == '__main__':
     main()
